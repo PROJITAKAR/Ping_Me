02db4ec6bb8d275273999256ba0684369fc89d54
PROJITA KAR <projita2003kar@gmail.com>
2025-07-20 01:21:36 +0530

    fix: corret cookie settings for cross-origin auth

--- Backend/controllers/AuthController.js
+++ Backend/controllers/AuthController.js
@@ -33,7 +33,7 @@ const RegisterController = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: true,
-      sameSite: "strict",
+      sameSite: "none",
       maxAge: 24 * 60 * 60 * 1000,
     });
 
@@ -94,7 +94,7 @@ const LoginController = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: true,
-      sameSite: "strict",
+      sameSite: "none",
       maxAge: 24 * 60 * 60 * 1000,
     });
 
